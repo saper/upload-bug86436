cb29d4be91a848b1f362fa2f1b61cfd73805381f
Marcin Cieslak <saper@saper.info>
2015-09-01 01:02:06 +0000

    Try upload

--- repr.sh
+++ repr.sh
@@ -7,6 +7,11 @@
 # Marcin Cieślak <saper@saper.info>, 2015
 # -------------------------------------------------------------------------
 
+# Need to provide the following files:
+# ca_user - central auth username
+# ca_token - central auth cookie
+# ca_userid - local wiki user id
+
 # https://commons.wikimedia.org/w/api.php?action=upload&filename=Old_and_New_London%2C_vol._1.djvu&format=json
 
 SITE=https://test2.wikipedia.org/w/api.php
@@ -16,6 +21,7 @@ FILE=Um_eine_Nasenlaenge_1949_Hans_Moser.ogv
 get() {
     tracelog="http.log.$1" ; shift
     curl --trace ${tracelog} \
+        -A "User:`cat ca_user` testing for https://phabricator.wikimedia.org/maniphest/task/edit/86436/" \
         --cacert GlobalSign.pem \
         -b "Cookie: test2wikiUserName=`cat ca_user`;test2wikiUserID=`cat ca_userid`;centralauth_User=`cat ca_user`;centralauth_Token=`cat ca_token`"  \
         "$@"
@@ -24,5 +30,10 @@ get() {
 get 01 "${SITE}?action=query&format=json&meta=tokens&type=csrf" | sed 's/^.*"csrftoken":"//;s/".*$//' > token
 # {"batchcomplete":"","query":{"tokens":{"csrftoken":"81bf356f1c123fb9888eb76e5d3e6cf955e4f237+\\"}}}
 cat token
-get 02 "${SITE}?action=query&format=json&meta=tokens&type=watch" | sed 's/^.*"watchtoken":"//;s/".*$//' > watchtoken
-cat watchtoken
+get 02  \
+    -F action=upload \
+    -F format=json \
+    -F filename=TestingNasenlaenge.ogv \
+    -F token=`cat token` \
+    -F file=@${FILE} \
+    ${SITE}
